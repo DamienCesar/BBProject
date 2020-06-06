530522460266f8ad4677b79c9150bc41fbba93b2
Damien CESAR <damien.cesar.dc@gmail.com>
2020-06-06 08:40:36 +0200

    commit

--- BrigadeBarber/app/src/main/java/com/dc/brigadebarber/MainActivity.kt
+++ BrigadeBarber/app/src/main/java/com/dc/brigadebarber/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.dc.brigadebarber
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.sql.Date
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.math.log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        val formatted = current.format(formatter)
+        val arrayAdapter: ArrayAdapter<*>
+        val time = arrayOf(
+            formatted
+        )
+
+        arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, time)
+        reservationList.adapter = arrayAdapter
+
+        calendarView.setOnDateChangeListener { calendarView, i, i2, i3 ->
+            Log.d("TEST","$i3 "+"$i2 "+"$i")
+        }
+
+
+
+
+
     }
 }
